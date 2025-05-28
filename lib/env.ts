20e645d503bdec15e79e552c6fe9410d95145683
barbar0j4v1er <barbar0j4v1er@null.computer>
2025-05-28 17:03:43 +0400

    :sparkles: setup db

--- lib/env.ts
+++ lib/env.ts
@@ -4,6 +4,11 @@ import tryParseEnv from "./try-parse-env";
 
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
+  TURSO_DATABASE_URL: z.string(),
+  TURSO_DATABASE_AUTH_TOKEN: z.string(),
+  CLOUDFLARE_ACCOUNT_ID: z.string().optional(),
+  CLOUDFLARE_DATABASE_ID: z.string().optional(),
+  CLOUDFLARE_D1_TOKEN: z.string().optional(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
