1901d321bbea2a315a53c1ab268ce4cac56b0146
Bárbaro Javier Valmaseda <41394649+barbar0jav1er@users.noreply.github.com>
2025-05-28 13:15:52 +0400

    :sparkles: add landing page (#50)

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  </div>
-  <button class="btn btn-secondary">
-    View Map <Icon name="tabler:map" size="32" />
-  </button>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ components/app/nav-bar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar bg-primary text-primary-content">
+    <div className="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </NuxtLink>
+    </div>
+    <div className="navbar-end">
+      <a className="btn btn-accent">Sign In</a>
+    </div>
+  </div>
+</template>
