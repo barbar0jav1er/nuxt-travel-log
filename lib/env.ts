ada54e06df19663a981881971d32ab964d30a154
barbar0j4v1er <barbar0j4v1er@null.computer>
2025-05-30 08:49:58 +0400

    :sparkles: setup auth

--- lib/env.ts
+++ lib/env.ts
@@ -6,6 +6,9 @@ const EnvSchema = z.object({
   NODE_ENV: z.string(),
   TURSO_DATABASE_URL: z.string(),
   TURSO_DATABASE_AUTH_TOKEN: z.string(),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.string(),
+  // TODO integrate with Cloudflare D1
   CLOUDFLARE_ACCOUNT_ID: z.string().optional(),
   CLOUDFLARE_DATABASE_ID: z.string().optional(),
   CLOUDFLARE_D1_TOKEN: z.string().optional(),
