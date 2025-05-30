c8da4841908a5175bac7496566df7d5162b00083
Bárbaro Javier Valmaseda <41394649+barbar0jav1er@users.noreply.github.com>
2025-05-30 10:05:55 +0400

    :sparkles: setup github auth (#55)

--- lib/env.ts
+++ lib/env.ts
@@ -8,6 +8,8 @@ const EnvSchema = z.object({
   TURSO_DATABASE_AUTH_TOKEN: z.string(),
   BETTER_AUTH_SECRET: z.string(),
   BETTER_AUTH_URL: z.string(),
+  AUTH_GITHUB_CLIENT_ID: z.string(),
+  AUTH_GITHUB_CLIENT_SECRET: z.string(),
   // TODO integrate with Cloudflare D1
   CLOUDFLARE_ACCOUNT_ID: z.string().optional(),
   CLOUDFLARE_DATABASE_ID: z.string().optional(),
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>You are log in</h1>
+</template>
