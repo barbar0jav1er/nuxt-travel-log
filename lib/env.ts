ff9cd4097e250285144ca75256b4ad0a37d269c2
Bárbaro Javier Valmaseda <41394649+barbar0jav1er@users.noreply.github.com>
2025-06-03 22:09:39 +0400

    :bug: fix deploy action

--- lib/env.ts
+++ lib/env.ts
@@ -3,12 +3,13 @@ import { z } from "zod";
 
 import tryParseEnv from "./try-parse-env";
 
+// TODO fix the default value on required env variables
 const EnvSchema = z.object({
   NODE_ENV: z.string().default("production"),
-  BETTER_AUTH_SECRET: z.string(),
-  BETTER_AUTH_URL: z.string(),
-  AUTH_GITHUB_CLIENT_ID: z.string(),
-  AUTH_GITHUB_CLIENT_SECRET: z.string(),
+  BETTER_AUTH_SECRET: z.string().default(""),
+  BETTER_AUTH_URL: z.string().default(""),
+  AUTH_GITHUB_CLIENT_ID: z.string().default(""),
+  AUTH_GITHUB_CLIENT_SECRET: z.string().default(""),
   CLOUDFLARE_ACCOUNT_ID: z.string().optional(),
   CLOUDFLARE_DATABASE_ID: z.string().optional(),
   CLOUDFLARE_D1_TOKEN: z.string().optional(),
@@ -19,7 +20,7 @@ export type EnvSchema = z.infer<typeof EnvSchema>;
 const defaultEnv = EnvSchema.parse(process.env);
 
 export function getEnv(cloudflareEnv?: Partial<Env>): EnvSchema {
-  if (cloudflareEnv && process.env.NODE_ENV !== "development") {
+  if (cloudflareEnv && process?.env?.NODE_ENV !== "development") {
     return EnvSchema.parse({
       ...process.env,
       ...cloudflareEnv,
